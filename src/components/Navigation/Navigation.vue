4d27210abd444891d3160853271b7497063738ea
Trevor <tdmichaelis@gmail.com>
2021-12-09 16:00:30 -0700

    ui changes to the navigation menu

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
 import IconLock from '~icons/tabler/lock'
+import IconFiles from '~icons/tabler/files'
+import IconBox from '~icons/tabler/box'
 import { isDark, toggleDark, useSdk } from '~/composables'
+
+type MenuLink = {
+  title: string
+  to?: string
+}
+
+type MenuItem = {
+  collapsible?: boolean
+  icon: any
+  links?: MenuLink[]
+  open?: boolean
+  title: string
+  to?: string
+}
+
 const emit = defineEmits(['close'])
 
 // const { t } = useI18n()
@@ -25,19 +42,18 @@ const signout = async () => {
   emit('close')
 }
 
-const links = [
-  { title: 'Form', to: '/pages/form' },
+const menuItems: MenuItem[] = [
+  { title: 'Form', to: '/pages/form', icon: IconLock },
   { title: 'Protected', to: '/pages/protected', icon: IconLock },
-]
-
-const menus = [
   {
     title: 'Pages',
+    icon: IconFiles,
     links: [{ title: 'Form', to: '/pages/form' }, { title: 'Protected', to: '/pages/protected' }],
   },
   {
+    collapsible: true,
     title: 'Components',
-    open: true,
+    icon: IconBox,
     links: [
       { title: 'Alert', to: '/components/alert' },
       { title: 'Avatar', to: '/components/alert' },
@@ -71,45 +87,61 @@ const menus = [
     ],
   },
   {
+    collapsible: true,
     title: 'Third Section',
+    icon: IconBox,
     links: [{ title: 'Menu #1' }, { title: 'Menu #2' }, { title: 'Menu #3' }],
   },
 ]
 </script>
 
 <template>
-  <router-link
-    v-for="(link, n) in links"
-    :key="n"
-    :to="link.to"
-    class="menu-link"
-  >
-    {{ link.title }}
-    <Component :is="link.icon" />
-  </router-link>
-  <Collapse
-    v-for="(menu, i) in menus"
-    :key="i"
-    v-model="menu.open"
-    group="menu"
-  >
-    <template #header="{ open }">
-      <div class="p-3 flex items-center gap-4">
-        <tabler-box />
-        <span class="font-bold flex-grow">{{ menu.title }}</span>
-        <tabler-chevron-right
-          class="transition duration-150 transform"
-          :class="{ 'rotate-90': open }"
-        />
-      </div>
-    </template>
+  <div v-for="(item, i) in menuItems" :key="i">
+    <Collapse
+      v-if="item.collapsible"
+      v-model="item.open"
+      group="menu"
+    >
+      <template #header="{ open }">
+        <div class="p-3 flex items-center gap-4">
+          <Component :is="item.icon" v-if="item.icon" />
+          <div v-else class="spacer w-5"></div>
+          <span class="font-bold flex-grow">{{ item.title }}</span>
+          <tabler-chevron-right
+            class="transition duration-150 transform"
+            :class="{ 'rotate-90': open }"
+          />
+        </div>
+      </template>
 
-    <div class="overflow-y-auto">
-      <div v-for="(link, n) in menu.links" :key="n" class="menu-link">
-        {{ link.title }}
+      <div class="overflow-y-auto">
+        <div v-for="(link, n) in item.links" :key="n" class="menu-link">
+          {{ link.title }}
+        </div>
+      </div>
+    </Collapse>
+    <router-link
+      v-else-if="item.to"
+      :to="item.to"
+      :class="item.icon ? 'p-3 flex items-center gap-4' : 'menu-link'"
+    >
+      <Component :is="item.icon" />
+      {{ item.title }}
+    </router-link>
+
+    <div v-else>
+      <div :class="item.icon ? 'p-3 flex items-center gap-4' : 'menu-link'">
+        <Component :is="item.icon" />
+        {{ item.title }}
+      </div>
+      <div class="flex flex-col">
+        <div v-for="(link, n) in item.links" :key="n" class="menu-link">
+          {{ link.title }}
+        </div>
       </div>
     </div>
-  </Collapse>
+  </div>
+
   <div class="flex-grow" />
   <Line class="lg:hidden" />
   <div class="flex items-center px-3 py-2 lg:hidden">
