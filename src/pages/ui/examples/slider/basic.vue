09a121230493abe6f8445cd983ce9947c43ebff1
Rob Taylor <roboncode@gmail.com>
2021-12-30 13:05:20 -0700

    updated slider

--- src/pages/ui/examples/slider/basic.vue
+++ src/pages/ui/examples/slider/basic.vue
@@ -1,55 +1,29 @@
 <script setup lang="ts">
-const curMinVal = ref(20)
-const curMaxVal = ref(180)
+const val = ref([40])
+const range = ref([40, 160])
 const busy = ref(false)
 let tooltipTimer: any
 
-watch(curMinVal, () => {
+watch(range, (newVal, oldVal) => {
   clearTimeout(tooltipTimer)
   setTimeout(() => busy.value = false, 1000)
-  if (!busy.value) busy.value = true
+  if (!busy.value && newVal[0] !== oldVal[0]) busy.value = true
 })
 </script>
 
 <template>
   <div class="grid gap-6">
-    <VuwiSlider
-      v-model:min="curMinVal"
-      v-model:max="curMaxVal"
-      :min-val="30"
-      :max-val="160"
-      root-class="vuwi-slider-sm"
-      range
-    ></VuwiSlider>
+    <VuwiSlider v-model="val" :min="0" :max="200" root-class="vuwi-slider" />
+    
+    <VuwiSlider v-model="range" :min="0" :max="200" root-class="vuwi-slider-sm" />
+    
+    <VuwiSlider v-model="range" :min="10" :max="190" />
+    
+    <VuwiSlider v-model="range" :min="20" :max="180" root-class="vuwi-slider-lg" />
+    
+    <VuwiSlider v-model="range" :min="10" :max="190" root-class="vuwi-slider-xl" />
 
-    <!-- <input type="range" :step="1" :min="20" :max="100" v-model="val" /> -->
-
-    <VuwiSlider v-model:min="curMinVal" v-model:max="curMaxVal" :min-val="20" :max-val="200"></VuwiSlider>
-
-    <VuwiSlider
-      v-model:min="curMinVal"
-      v-model:max="curMaxVal"
-      :min-val="0"
-      :max-val="200"
-      root-class="vuwi-slider-lg"
-    ></VuwiSlider>
-
-    <VuwiSlider
-      v-model:min="curMinVal"
-      v-model:max="curMaxVal"
-      :min-val="0"
-      :max-val="200"
-      root-class="vuwi-slider-xl"
-    ></VuwiSlider>
-
-    <VuwiSlider
-      v-model:min="curMinVal"
-      v-model:max="curMaxVal"
-      :min-val="0"
-      :max-val="200"
-      root-class="vuwi-slider-xl"
-      range
-    >
+    <VuwiSlider v-model="range" :min="0" :max="200" root-class="vuwi-slider-xl">
       <template #highlight>
         <div class="h-full w-full vuwi-progress-stripe vuwi-progress bg-yellow-500 rounded-full"></div>
       </template>
@@ -57,14 +31,16 @@ watch(curMinVal, () => {
       <template #thumb-left>
         <transition name="fade-up">
           <div v-if="busy" class="block absolute bottom-12">
-            <div class="text-center bg-black px-2 py-1 font-bold text-white text-sm rounded-lg min-w-10">
-              <span>{{ curMinVal }}</span>
+            <div
+              class="slider-tooltip"
+            >
+              <span>{{ range[0] }}</span>
             </div>
           </div>
         </transition>
         <div v-if="busy" class="w-10 h-10 flex items-center justify-center text-white">
           <div
-            class="vuwi-spinner w-8 h-8 min-w-8 min-h-8 border-4 border-blue-400 border-r-transparent"
+            class="vuwi-spinner slider-busy"
           />
         </div>
       </template>
@@ -72,10 +48,19 @@ watch(curMinVal, () => {
       <template #thumb-right>
         <div
           class="w-10 h-10 flex items-center justify-center bg-yellow-600 rounded-full text-white font-bold text-sm"
-        >
-          {{ curMaxVal }}
-        </div>
+        >{{ range[1] }}</div>
       </template>
     </VuwiSlider>
   </div>
 </template>
+
+
+<style>
+.slider-tooltip {
+  @apply text-center bg-black px-2 py-1 font-bold text-white text-sm rounded-lg min-w-10;
+}
+
+.slider-busy {
+  @apply w-8 h-8 min-w-8 min-h-8 border-4 border-blue-400 border-r-transparent;
+}
+</style>
