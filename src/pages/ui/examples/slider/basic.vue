3b0b1d5cac5c745a95d65b0a2351e10fb028c2e1
Rob Taylor <roboncode@gmail.com>
2021-12-30 11:20:19 -0700

    slider updates

--- src/pages/ui/examples/slider/basic.vue
+++ src/pages/ui/examples/slider/basic.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-const minVal = ref(20)
-const maxVal = ref(180)
-
+const curMinVal = ref(20)
+const curMaxVal = ref(180)
 const busy = ref(false)
 let tooltipTimer: any
 
-watch(minVal, () => {
+watch(curMinVal, () => {
   clearTimeout(tooltipTimer)
   setTimeout(() => busy.value = false, 1000)
   if (!busy.value) busy.value = true
@@ -15,37 +14,41 @@ watch(minVal, () => {
 <template>
   <div class="grid gap-6">
     <VuwiSlider
-      v-model:min="minVal"
-      v-model:max="maxVal"
-      :min-val="0"
-      :max-val="200"
+      v-model:min="curMinVal"
+      v-model:max="curMaxVal"
+      :min-val="30"
+      :max-val="160"
       root-class="vuwi-slider-sm"
+      range
     ></VuwiSlider>
 
-    <VuwiSlider v-model:min="minVal" v-model:max="maxVal" :min-val="0" :max-val="200"></VuwiSlider>
+    <!-- <input type="range" :step="1" :min="20" :max="100" v-model="val" /> -->
+
+    <VuwiSlider v-model:min="curMinVal" v-model:max="curMaxVal" :min-val="20" :max-val="200"></VuwiSlider>
 
     <VuwiSlider
-      v-model:min="minVal"
-      v-model:max="maxVal"
+      v-model:min="curMinVal"
+      v-model:max="curMaxVal"
       :min-val="0"
       :max-val="200"
       root-class="vuwi-slider-lg"
     ></VuwiSlider>
 
     <VuwiSlider
-      v-model:min="minVal"
-      v-model:max="maxVal"
+      v-model:min="curMinVal"
+      v-model:max="curMaxVal"
       :min-val="0"
       :max-val="200"
       root-class="vuwi-slider-xl"
     ></VuwiSlider>
 
     <VuwiSlider
-      v-model:min="minVal"
-      v-model:max="maxVal"
+      v-model:min="curMinVal"
+      v-model:max="curMaxVal"
       :min-val="0"
       :max-val="200"
       root-class="vuwi-slider-xl"
+      range
     >
       <template #highlight>
         <div class="h-full w-full vuwi-progress-stripe vuwi-progress bg-yellow-500 rounded-full"></div>
@@ -55,7 +58,7 @@ watch(minVal, () => {
         <transition name="fade-up">
           <div v-if="busy" class="block absolute bottom-12">
             <div class="text-center bg-black px-2 py-1 font-bold text-white text-sm rounded-lg min-w-10">
-              <span>{{ minVal }}</span>
+              <span>{{ curMinVal }}</span>
             </div>
           </div>
         </transition>
@@ -70,7 +73,7 @@ watch(minVal, () => {
         <div
           class="w-10 h-10 flex items-center justify-center bg-yellow-600 rounded-full text-white font-bold text-sm"
         >
-          {{ maxVal }}
+          {{ curMaxVal }}
         </div>
       </template>
     </VuwiSlider>
